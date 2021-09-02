9437d0eb2688b0f8a25a0ec0bc2e8a5dba21ecbb
hassen zeighoum <hassen.zeighm@gmail.com>
2021-09-02 10:27:09 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CatalogueService} from "./catalogue.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ecom-web';
+export class AppComponent implements OnInit{
+  public categories: any;
+  public err: any;
+  constructor(private catServices: CatalogueService) {
+  }
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+  private getCategories() {
+      this.catServices.getResource("/categories")
+        .subscribe(data=>{
+          this.categories=data;
+        },error => {
+          console.log(this.err);
+        })
+  }
 }
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {CatalogueService} from "../catalogue.service";
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  public products: any;
+  private err: any;
+
+  constructor(private catService: CatalogueService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  private getProducts(){
+      this.catService.getResource("/products")
+        .subscribe(data=>{
+          this.products=data;
+        }, error => {
+          console.log(this.err);
+        })
+  }
+}
